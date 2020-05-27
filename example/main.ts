35ed2fd2acc910a0485940d289acabf4bebd7b15
Isham Mohamed <IshamMohamed@users.noreply.github.com>
2020-05-27 16:59:44 +0800

    Update main.ts

--- example/main.ts
+++ example/main.ts
@@ -2,7 +2,7 @@ import { serve } from "./deps.ts";
 
 const PORT = 1993;
 const s = serve(`0.0.0.0:${PORT}`);
-const body = new TextEncoder().encode("Hello World\n");
+const body = new TextEncoder().encode("Hello Welcome to Deno 🦕 on Azure\n");
 
 console.log(`Server started on port ${PORT}`);
 for await (const req of s) {
